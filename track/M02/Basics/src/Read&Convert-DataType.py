56e4391c54540c7c37c727bfdb0d7782be6ec8f4
Navin A <navinashok13@gmail,com>
2026-08-04 16:24:30 +0530

    Convert datatype

---
+++ track/M02/Basics/src/Read&Convert-DataType.py
@@ -0,0 +1,17 @@
+# Read and convert the student details
+name=input()
+age=input()
+age=int(age)
+rating=input()
+rating=float(rating)
+print("Student:",name)
+print("Age:",age)
+print("Rating:",rating)
+print(type(name))
+print(type(age))
+print(type(rating))
+
+# Display the values
+
+
+# Display the data types
