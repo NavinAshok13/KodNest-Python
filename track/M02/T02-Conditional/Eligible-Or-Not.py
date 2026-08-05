8fc743b66bb15bf58637720634ef746cbd49460a
Navin A <navinashok13@gmail,com>
2026-08-05 15:48:50 +0530

    changes

--- track/M02/T02-Conditional/Eligible-Or-Not.py
+++ track/M02/T02-Conditional/Eligible-Or-Not.py
@@ -1,6 +1,6 @@
 marks=int(input("Enter marks: "))
 atten=int(input("Enter attendance: "))
-pro=input("Enter project completion status (yes/no): ")
+pro=input("Enter project completion status (yes/no): ").lower()
 if marks>=60:
      if atten>=75:
         if pro=="yes":
